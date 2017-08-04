2548638b45f967e2e0f43677824656b1aafbc233
Sean Shookman <sshookman@cars.com>
2017-08-04 16:19:26 -0500

    Styling and news tweaks

--- packages/default/widgets/headlines/headlines.js
+++ packages/default/widgets/headlines/headlines.js
@@ -2,19 +2,21 @@ widget = {
 
   onData: function (el, data) {
     if (data.news && data.news.status === "ok") {
-      
+
         var $content = $('.content', el);
         $content.empty();
 
         if (data.news.articles.length) {
-          data.news.articles.forEach(function (article) {
+          for (var i = 0; i < 6; i++) {
+            var article = data.news.articles[i];
             $content.append(
                 "<div class='news-story'>" +
+                  "<img class='thumb' src='" + article.urlToImage + "'></img>" +
                   "<div class='title'>" + article.title + "</div>" +
-                  "<div class='description'>" + article.description + "</div>" +
-                "</div>"
+                "</div>" +
+                "<div class='description'>" + article.description + "</div>"
             );
-          })
+          }
         }
     }
   }
--- packages/default/widgets/weather/weather.js
+++ packages/default/widgets/weather/weather.js
@@ -2,24 +2,22 @@ widget = {
 
   onData: function (el, data) {
     if (data.weather) {
-      
-        var $content = $('.content', el);
+
+        var $content = $('.weather', el);
         $content.empty();
 
         var textForecast = data.weather.forecast.txt_forecast.forecastday;
 
         if (textForecast.length) {
-            
-            $content.append("<ul class='weather-forecast'>");
+
             textForecast.forEach(function (forecast) {
                 $content.append(
-                    "<li class='forecast'>" +
+                    "<div class='forecast'>" +
                       "<div class='title'>" + forecast.title + "</div>" +
                       "<div class='details'>" + forecast.fcttext + "</div>" +
-                    "</li>"
+                    "</div>"
                 );
-            })
-            $content.append("</ul>");
+            });
         }
     }
   }
